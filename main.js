5bd53ca1837cecda6363c38782f08af0dae90575
AsmaaZeyada <bmaser38@gmail.com>
2022-09-17 19:30:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector(".menu-icon");
+let ulNav = document.querySelector(".ul-nav");
+let header = document.querySelector("header");
+let headerHeight = header.getBoundingClientRect().height;
+let homeHeight = document.querySelector("#home").getBoundingClientRect().height;
+let up = document.querySelector(".up");
+
+menu.addEventListener("click", function() {
+	if(ulNav.classList.contains("open-menu")) {
+		ulNav.classList.remove("open-menu")
+	} else {
+		ulNav.classList.add("open-menu")
+	}
+})
+
+ulNav.addEventListener("click", function() {
+	ulNav.classList.remove("open-menu")
+	
+})
+
+document.addEventListener("scroll", () => {
+	if (window.pageYOffset + 50 > homeHeight) {
+		up.classList.add("visible");
+		header.classList.add("fixed")
+	} else {
+		up.classList.remove("visible");
+		header.classList.remove("fixed")
+	}
+})
